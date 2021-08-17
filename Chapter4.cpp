c466bbee1c14b425be528fb10b6d8aad69f1ba77
BrainSoup <soulkeep1989@gmail.com>
2021-08-17 03:48:54 +0300

    Added switch example

--- Chapter4.cpp
+++ Chapter4.cpp
@@ -8,6 +8,7 @@ void Chapter4Func()
 {
     cout << endl << "Chapter 4. Loops" << endl;
 
+    //Loops
     cout << "for(int i=0; i<3; i++)" << endl;
     for (int i = 0; i < 3; i++)
         cout << i << " ";
@@ -21,4 +22,25 @@ void Chapter4Func()
     cout << "for (int i = 0, j = 0; i < 3; i++, ++j)" << endl;
     for (int i = 0, j = 0; i < 3; i++, ++j)
         cout << "i=" << i << " j=" << j << endl;
+    
+    //Also there is while() and do{}while()
+    
+    //Switch
+    int n = 1, skip = 1;
+    cout << "Switch(n): n=" << n << endl;
+    switch (n) {
+        case 0: cout << 1;
+            break;  //stops execution of switch
+        case 1: cout << 1;
+            if (!skip)  //skips "cout << 2;" cause "case 2:" is not instruction, it's just label
+        case 2: 
+            cout << 2;
+            if (!skip)
+                break;  //Whithout "break;" instruction switch continue execution of next inctructions
+        case 3:
+            cout << 3;
+
+        default: cout << 4;
+    }
+    cout << endl;
 }
