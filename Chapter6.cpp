a07261654fb99f352647b41eef5c0461178cbab6
BrainSoup <soulkeep1989@gmail.com>
2021-08-17 04:37:45 +0300

    Added examples with pointer substruction

--- Chapter6.cpp
+++ Chapter6.cpp
@@ -18,15 +18,16 @@ void Chapter6Func()
     //Pointer declaration
     int* i_ptr;
 
+    int i = 10;
     //Pointer operators
     i_ptr = &i; // take address from variable i
     i = *i_ptr; // take value from pointer
 
     //Forbidden to address different types
     double d = 1;
-    //i_ptr = &d; // cannot convert from 'double *' to 'int *'
+    //i_ptr = &d; // cannot convert from 'double *' to 'int *', need explicit conversation
     //double* d_ptr = &i; //a value of type "int *" cannot be used to initialize an entity of type "double *"
-    //But it's possible to convert mannualy
+    //But it's possible to make explicit conversation
     i_ptr = (int*)&d;
 
     //Assign value to pointer
@@ -35,9 +36,19 @@ void Chapter6Func()
     cout << "(*i_ptr = 11); i = " << i << endl;
     (*i_ptr)++; //Round brackets required cause of operation priority
     cout << "(*i_ptr)++; i = " << i << endl;
+    
+    //Pointers and massives
+    cout << "\nPointers and massives: " << endl;
+    int massive[10] = {};
+    int *m_ptr = massive;   //not indexed massive name generate const pointer to 0 element
+    OutputMass(massive, 10);    //use massive as pointer parameter
+    massive[4] = 4; m_ptr[5] = 5;   //pointers can use indexers like massives
+    *(m_ptr + 2) = 2;   //same instruction as above
+    OutputMass(m_ptr, 10);
+    int* m[10]; //massive of int pointers
 
     //Possible pointer ariphmetic operations
-    cout << "Pointer ariphmetic operations: " << endl;
+    cout << "\nPointer ariphmetic operations: " << endl;
     i_ptr = &i;
     cout << "i_ptr = " << i_ptr << endl;
     i_ptr++;
@@ -47,18 +58,9 @@ void Chapter6Func()
     i_ptr+=5;
     cout << "i_ptr+=5 : " << i_ptr << endl;
     cout << "i_ptr-&i : " << i_ptr - &i << endl;
-    //i_ptr + &i; //Error
-
-    //Pointers and massives
-    cout << "Pointers and massives: " << endl;
-    int massive[10] = {};
-    int *m_ptr = massive;   //not indexed massive name generate const pointer to 0 element
-    OutputMass(massive, 10);    //use massive as pointer parameter
-    massive[4] = 4; m_ptr[5] = 5;   //pointers can use indexers like massives
-    *(m_ptr + 2) = 2;   //same instruction as above
-    OutputMass(m_ptr, 10);
-
-    int* m[10]; //massive of int pointers
+    //i_ptr + &i; //Error, pointer sum is prohibited
+    cout << "&massive[6]-massive : " << &massive[6] - massive << endl;
+    cout << "(char*)&massive[6] - (char*)massive : " << (char*)&massive[6] - (char*)massive << endl;
 
     //level of pointers
     int x, * p, ** q; //int, int *, int **
