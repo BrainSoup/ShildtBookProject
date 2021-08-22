48a5374607cc91be7ea060794f1824b013de49fb
BrainSoup <soulkeep1989@gmail.com>
2021-08-22 23:04:03 +0300

    Add chapter 12, fix name intersection

--- Chapter11.cpp
+++ Chapter11.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-class SimpleClass {
+class SimpleClass11 {
 //private:
 	int a, b;
 public:
 	int* c = 0;
-	SimpleClass();
-	SimpleClass(int a); //	for one param constructor compiler create implicit conversation from param type to class type
-	SimpleClass(int a, int b);
-	~SimpleClass();
+	SimpleClass11();
+	SimpleClass11(int a); //	for one param constructor compiler create implicit conversation from param type to class type
+	SimpleClass11(int a, int b);
+	~SimpleClass11();
 	void SomeMethod();	//Not inline
 	int Get_a() { return a; }	//inline cause declared in class declaration
 }global_simpleClass;	//Constructors of global objects executes before main()
@@ -19,84 +19,84 @@ public:
 //Classes
 void Chapter11Func()
 {
-	PrintColorText("\nChapter 11. Classes");
+	PrintColorText("\nChapter 11. Classes (Basics)");
 
 	//Initialize new class
 	cout << "SimpleClass c = SimpleClass(); \n";
-	SimpleClass c = SimpleClass();
+	SimpleClass11 c = SimpleClass11();
 
 	cout << "SimpleClass *c_ptr = new SimpleClass(); \n";
-	SimpleClass *c_ptr = new SimpleClass();
+	SimpleClass11 *c_ptr = new SimpleClass11();
 	delete c_ptr;
 
 	cout << "SimpleClass c1(1) \nSimpleClass c1_1{2, 3}; \nSimpleClass c1_2;\n";
 	cout << "static_cast<SimpleClass>(c1_2); \nnew SimpleClass(1, 2);" << endl;
 	cout << "\n1) direct-initialization" << endl;
-	SimpleClass c1(1);
-	SimpleClass c1_1{ 2, 3 };
-	SimpleClass c1_2;
+	SimpleClass11 c1(1);
+	SimpleClass11 c1_1{ 2, 3 };
+	SimpleClass11 c1_2;
 	//static_cast<SimpleClass>(c1_2); //no constructor call, but there is destructor call
-	new SimpleClass(4, 5);
-	(SimpleClass)6; // Only for 1 param constructor
+	new SimpleClass11(4, 5);
+	(SimpleClass11)6; // Only for 1 param constructor
 	//Some other cases
-	SimpleClass c1_3();	//No compile error, DO NOTHING, c1_3 not declared
+	SimpleClass11 c1_3();	//No compile error, DO NOTHING, c1_3 not declared
 
 	cout << "\n2) copy-initialization" << endl;
 	cout << "SimpleClass c2 = 2; \nSimpleClass c2_1 = { 3, 4 }; \nSimpleClass c2_2 = SimpleClass{}" << endl;
-	SimpleClass c2 = 2;	//	In this case compiler create implicit conversation from param type to class type
-	SimpleClass c2_1 = { 3, 4 };
-	SimpleClass c2_2 = SimpleClass{};
+	SimpleClass11 c2 = 2;	//	In this case compiler create implicit conversation from param type to class type
+	SimpleClass11 c2_1 = { 3, 4 };
+	SimpleClass11 c2_2 = SimpleClass11{};
 	//Also passing object to function or returning from it
 	//Some other cases
 
 	cout << "\n3) list-initialization" << endl;
 	cout << "SimpleClass c3 = {5}; \nSimpleClass c3{ 3, 4 }; \nSimpleClass{};" << endl;
-	SimpleClass c3 = {5};
-	SimpleClass c3_1{ 3, 4 };
-	SimpleClass{};
+	SimpleClass11 c3 = {5};
+	SimpleClass11 c3_1{ 3, 4 };
+	SimpleClass11{};
 	/*	Also passing {arguments} to function or returning from it
 	1)  SimpleClass func(SimpleClass param) {return {1};}
 	2)	func({1,2}); */
 
 	cout << "\nSimpleClass mass_c[3] = { 1, {}, {3,4} }; \n";
-	SimpleClass mass_c[3] = { 1, {}, {3,4} };
+	SimpleClass11 mass_c[3] = { 1, {}, {3,4} };
 }
 
 //Constructor
-SimpleClass::SimpleClass()
+SimpleClass11::SimpleClass11()
 {
-	cout << "Constructor SimpleClass()" << endl;
+	cout << "Constructor SimpleClass11()" << endl;
 	a = 1;
 	b = 2;
 	c = new int(0);
 }
 
 //Parametrised constructor
-SimpleClass::SimpleClass(int A)
+SimpleClass11::SimpleClass11(int A)
 {
-	cout << "Constructor SimpleClass(" << A << ")" << endl;
+	cout << "Constructor SimpleClass11(" << A << ")" << endl;
 	a = A;
 	b = 2;
 	c = new int(0);
 }
 
 //Parametrised constructor2
-SimpleClass::SimpleClass(int A, int B)
+SimpleClass11::SimpleClass11(int A, int B)
 {
-	cout << "Constructor SimpleClass(" << A << ", " << B << ")" << endl;
+	cout << "Constructor SimpleClass11(" << A << ", " << B << ")" << endl;
 	a = A;
 	b = 2;
 	c = new int(0);
 }
 
 //Destructor
-SimpleClass::~SimpleClass()
+SimpleClass11::~SimpleClass11()
 {
 	if (c)
 		delete(c);
 }
 
-void SimpleClass::SomeMethod()
+void SimpleClass11::SomeMethod()
 {
 	a = 0;
 	b = 0;
--- Chapter12.cpp
+++ Chapter12.cpp
@@ -1,58 +1,151 @@
 #include <iostream>
 #include "ColorText.h"
+using namespace std;
 
 //TODO virtual functions from constructor
 
+class SimpleClass12;	//TODO explain this
+
+//Friend class
+class FriendClass {
+public:
+	void SomeMethod(SimpleClass12 *c);
+	FriendClass() { cout << "FriendClass Constructor" << endl; };
+	FriendClass(const FriendClass & copy) { cout << "FriendClass Copy Constructor" << endl; };
+	~FriendClass() { cout << "FriendClass Destructor" << endl; };
+};
+
 class SimpleClass12 {
-	//private:
-	int a, b;
+	int pr_a = 0, pr_b = 0;
 public:
 	int* c = 0;
-	SimpleClass12();
+	char * name = 0;
 	SimpleClass12(int a);
+	SimpleClass12(int a, int b, int c, const char * name);
 	~SimpleClass12();
-	void SomeMethod();//Not inline
-	int Get_a() { return a; }//inline
+	const char *ToString();
+	friend void Print(SimpleClass12 &c);
+	friend void FriendClass::SomeMethod(SimpleClass12* c);
+	friend int Sum(SimpleClass12* c);
 };
+
+//Friend function
+void Print(SimpleClass12 &c)
+{
+	printf("%s = ( pr_a = %d, pr_b = %d, c = %d)\n", c.name ? c.name : "null", c.pr_a, c.pr_b, c.c ? *(c.c) : 0);
+}
+
+//Object params by value Example
+void ObjValueParam(SimpleClass12 c)	//Destructor will be called for param c
+{
+	cout << "ObjValueParam()" << endl;
+}
+
+//Object params by value Example
+SimpleClass12 ObjValueReturn()
+{
+	cout << "ObjValueReturn()" << endl;
+	SimpleClass12 c4(4);	//Destructor will be called for c4
+	return c4;
+}
+
+//Copy constructor example
+FriendClass CopyConstructorTest(FriendClass c)
+{
+	cout << "CopyConstructorTest()" << endl;
+	FriendClass c1;		//Constructor
+	cout << 1 << endl;
+	c1 = c;				//Any Constructor not called
+	cout << 2 << endl;
+	FriendClass c2 = c;	//Explicit initialisation (Copy Constructor)
+	cout << 3 << endl;
+						//TODO Why return don't execute Copy Constructor?
+	return c2;			//Return (No Constructor) 
+}
 
 //Classes
 void Chapter12Func()
 {
-	//Initialize new class
-	SimpleClass12 c1 = SimpleClass12();
-	SimpleClass12* c2 = new SimpleClass12();
-	SimpleClass12 c3(), c5(1);
-	SimpleClass12 c4 = 4;//Only if available 1 param constructor
-	SimpleClass12 mass_c[3] = { 1,2,3 };//Only if available 1 param constructor
+	PrintColorText("\nChapter 12. Classes (Advanced)");
+
+	//Friend example
+	cout << "Friend functions and methods:" << endl;
+	SimpleClass12 c1 = SimpleClass12(1, 2, 3, "c1");
+	Print(c1);
+
+	FriendClass fc;
+	fc.SomeMethod(&c1);
+
+	//Object copying
+	cout <<"\nObject copying:"<< endl;
+	SimpleClass12 c2(2, 1, 1, "c2");
+	Print(c1); Print(c2);
+	//cout << "c1 = " << (c1.Print(), "; c2 = ") << (c2.Print(),"") << endl;
+	c2 = c1;	//By default copy all fields from c1 to c2
+	cout << "c2 = c1;";
+	Print(c2);
+	cout << "c2.c == c1.c;" << " c2.c = " << c2.c << " c1.c = " << c1.c << endl;
+	c2.c = 0; //Fix double memory release
+	c2.name = 0; //Fix double memory release
+
+	//Sending and returning objects by value
+	cout << "\nSending and returning objects by value" << endl;
+	SimpleClass12 c3 = SimpleClass12(3);
+	ObjValueParam(c3);						// Destructor will be called for c3 and for param in ObjValueParam() with same fields
+	SimpleClass12 c5 = ObjValueReturn();	// Destructor will be called for c5 and c4 wich have copied fields
+	
+	//Copy Constructor
+	cout << "\nCopy Constructor" << endl;
+	FriendClass fc1;				//Constructor
+	FriendClass fc2 =				//Explicit initialisation (Copy Constructor)
+		CopyConstructorTest(fc1);	//Parameter initialisation (Copy Constructor)
 
 }
 
-//Constructor
-//Constructors of global objects executes before main()
-SimpleClass12::SimpleClass12()
+SimpleClass12::SimpleClass12(int a)
 {
-	a = 1;
-	b = 2;
-	c = new int(0);
+	pr_a = a;
 }
 
-//Parametrised constructor
-SimpleClass12::SimpleClass12(int A)
+//TODO fix memory leak
+const char *SimpleClass12::ToString()
 {
-	a = A;
-	b = 2;
-	c = new int(0);
+	const size_t size = 40;
+	char *result = new char[size];
+	snprintf(result, size, "( pr_a = %d, pr_b = %d, c = %d)", pr_a, pr_b, *c);
+	return result;
 }
 
-//Destructor
+SimpleClass12::SimpleClass12(int a, int b, int c, const char * name_)
+{
+
+	cout << "Constructor " << (name_ ? name_ : "null" )<< " (int a, int b, int c)" << endl;
+	pr_a = a;
+	pr_b = b;
+	this->c = new int(c);
+	if (name_)
+	{
+		size_t size = strlen(name_) + 1;
+		name = new char[size];
+		strcpy_s(name, size, name_);
+	}
+}
+
 SimpleClass12::~SimpleClass12()
 {
+	cout << "Destructor() " << pr_a << " ";
+	if (name)
+	{
+		cout << name << endl;
+		delete[](name);
+	}
+	else
+		cout << endl;
 	if (c)
 		delete(c);
 }
 
-void SimpleClass12::SomeMethod()
+void FriendClass::SomeMethod(SimpleClass12* c)
 {
-	a = 0;
-	b = 0;
+	cout << "FriendClass::SomeMethod(SimpleClass12* c), c->pr_a = " << c->pr_a << ", c->pr_b = " << c->pr_b << endl;
 }
